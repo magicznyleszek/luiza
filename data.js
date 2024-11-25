65d36ed23a854de0bf74659b45dc2d69ab546a23
Leszek <smutnyleszek@gmail.com>
2024-11-25 16:10:06 +0100

    wip

--- data.js
+++ data.js
@@ -1,10 +1,16 @@
 ---
 ---
 (() => {
-  window.gepoza = {{ site.data.zawody | jsonify }};
+  window.gepoza = {{ site.data.zawody | jsonify }} || [];
   
+  function renderRandomIdea() {
+    const randomItem = window.gepoza[Math.floor(Math.random() * window.gepoza.length)];
+    console.log(randomItem);
+  }
+
   window.addEventListener('randomize', (event) => {
-    alert(window.gepoza?.[0]);
+    console.info('randomize');
+    renderRandomIdea();
   });
 
   console.info('ready');
