6a91d674b1e55b503e41957b8fcf3392b1341efe
Leszek <smutnyleszek@gmail.com>
2024-11-25 16:15:06 +0100

    print stuff

--- data.js
+++ data.js
@@ -6,6 +6,10 @@
   function renderRandomIdea() {
     const randomItem = window.gepoza[Math.floor(Math.random() * window.gepoza.length)];
     console.log(randomItem);
+    const out = document.getElementById('out');
+    if (out) {
+      out.innerHTML = JSON.stringify(randomItem);
+    }
   }
 
   window.addEventListener('randomize', (event) => {
