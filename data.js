de9a4d3114d968dee42735bd0efbd15c86407552
Leszek <smutnyleszek@gmail.com>
2024-11-26 22:29:17 +0100

    use csv file with a long (WIP/unchecked) list of job names

--- data.js
+++ data.js
@@ -1,15 +1,26 @@
 ---
 ---
 (() => {
+  // This line is Jekyll x JS code:
   window.gepoza = {{ site.data.zawody | jsonify }} || [];
+
+  // Verify if `name_m,name_f,description,source` row is present in `zawody.csv` file.
+  if ('name_m' in window.gepoza[0] === false) {
+    console.error('not ready');
+    throw new Error('Did you forgot to add a mandatory header row in your CSV file? Yes, you did!');
+  }
   
   function renderRandomIdea() {
-    const randomItem = window.gepoza[Math.floor(Math.random() * window.gepoza.length)];
+    const item = window.gepoza[Math.floor(Math.random() * window.gepoza.length)];
     const nameEl = document.getElementById('js-name');
     const descriptionEl = document.getElementById('js-description');
     if (nameEl && descriptionEl) {
-      nameEl.innerText = randomItem.name.join('/');
-      descriptionEl.innerText = randomItem.description;
+      let fullName = item.name_m;
+      if (item.name_f) {
+        fullName += `/${item.name_f}`;
+      }
+      nameEl.innerText = fullName;
+      descriptionEl.innerText = item.description;
     }
   }
 
