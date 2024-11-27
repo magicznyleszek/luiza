e5ec63b4efd8a746d470ebb328102336e5617fe0
Leszek <smutnyleszek@gmail.com>
2024-11-27 13:37:07 +0100

    move jekyll code outside script file

--- data.js
+++ data.js
@@ -1,34 +1,35 @@
----
----
 (() => {
-  // This line is Jekyll x JS code:
-  window.gepoza = {{ site.data.zawody | jsonify }} || [];
-
   // Verify if `name_m,name_f,description,source` row is present in `zawody.csv` file.
   if ('name_m' in window.gepoza[0] === false) {
-    console.error('not ready');
+    console.error('definitely not ready');
     throw new Error('Did you forgot to add a mandatory header row in your CSV file? Yes, you did!');
   }
   
+  /**
+   * Fetches random item from the big list of ideas, and then renders it in UI.
+   */
   function renderRandomIdea() {
-    const item = window.gepoza[Math.floor(Math.random() * window.gepoza.length)];
+    const idea = window.gepoza[Math.floor(Math.random() * window.gepoza.length)];
+    console.info('new idea:', idea);
     const nameEl = document.getElementById('js-name');
     const descriptionEl = document.getElementById('js-description');
     if (nameEl && descriptionEl) {
-      let fullName = item.name_m;
-      if (item.name_f) {
-        fullName += `/${item.name_f}`;
+      let fullName = idea.name_m;
+      if (idea.name_f) {
+        fullName += `/${idea.name_f}`;
       }
       nameEl.innerText = fullName;
-      descriptionEl.innerText = item.description;
+      descriptionEl.innerText = idea.description;
     }
   }
 
+  // Listen to event dispatched by button in UI.
   window.addEventListener('randomize', (event) => {
-    console.info('randomize');
+    console.info('randomize event received');
     renderRandomIdea();
   });
 
+  // Load one idea on start.
   window.addEventListener('load', renderRandomIdea);
 
   console.info('ready');
