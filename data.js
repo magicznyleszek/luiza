020755be764474a9fe9e29dc31d813208120f327
Leszek <smutnyleszek@gmail.com>
2024-12-02 23:40:10 +0100

    rename gepoza to luiza

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 (() => {
   // Verify if `name_m,name_f,description,source` row is present in `zawody.csv` file.
-  if ('name_m' in window.gepoza[0] === false) {
+  if ('name_m' in window.luiza[0] === false) {
     console.error('definitely not ready');
     throw new Error('Did you forgot to add a mandatory header row in your CSV file? Yes, you did!');
   }
@@ -9,7 +9,7 @@
    * Fetches random item from the big list of ideas, and then renders it in UI.
    */
   function renderRandomIdea() {
-    const idea = window.gepoza[Math.floor(Math.random() * window.gepoza.length)];
+    const idea = window.luiza[Math.floor(Math.random() * window.luiza.length)];
     console.info('new idea:', idea);
     const nameEl = document.getElementById('js-name');
     const descriptionEl = document.getElementById('js-description');
